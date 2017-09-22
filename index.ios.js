8740f0ee5ffc42e11cbc2fbcf7a000d5cad2818d
Ginny Fahs <ginnyfahs@gmail.com>
2017-09-21 18:24:51 -0700

    Use react-native-dotenv and make api call for test location data

--- index.ios.js
+++ index.ios.js
@@ -10,11 +10,13 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  Button,
   View
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 // import SearchBox from SearchBox;
-
+import axios from 'axios';
+import {REACT_APP_GOOGLE_MAPS_API, REACT_APP_GOOGLE_PLACES_API} from 'react-native-dotenv';
 
 export default class CatCallOutApp extends Component {
   constructor(props) {
@@ -31,6 +33,18 @@ export default class CatCallOutApp extends Component {
       locationInput: textInput
     });
   }
+
+  handleSubmit(textInput) {
+    axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=" + REACT_APP_GOOGLE_PLACES_API)
+    .then(response => console.log(response))
+    .catch(error => console.log("Failjax: ", error))
+  }
+
+  // componentDidMount() {
+  //   axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=" + GOOGLE_PLACES_API)
+  //   .then(response => console.log(response))
+  //   .catch(error => console.log("Failjax: ", error))
+  // }
 
   render() {
     return (
@@ -50,6 +64,7 @@ export default class CatCallOutApp extends Component {
             style={ styles.input }
             onChangeText={this.handleLocationInput}
             value={this.state.locationInput}
+            onSubmitEditing={this.handleSubmit}
           />
         </View>
       </MapView>
@@ -75,7 +90,8 @@ const styles = StyleSheet.create({
     elevation: 1,
     backgroundColor: 'white',
     top: 50,
-    width: '70%'
+    width: '70%',
+    height: 200
   }
 });
 
