940db979c4709d7d71895cce6d2ba9a6a5bd48d2
Josh Leichtung <joshleichtung@gmail.com>
2017-09-22 10:19:02 -0700

    Run react-native upgrade to relink

--- ios/CatCallOutApp/AppDelegate.m
+++ ios/CatCallOutApp/AppDelegate.m
@@ -12,15 +12,11 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-@import GoogleMaps;
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  
-  [GMSServices provideAPIKey:@CcoGoogleMapsAPIKey];
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
