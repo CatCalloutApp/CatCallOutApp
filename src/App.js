9f2119e18908c508c002f26fa475654c0684d954
Ginny Fahs <ginnyfahs@gmail.com>
2017-10-06 11:47:44 -0700

    Implemented skeleton for react-native-router-flux

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import MapScreen from './Components/MapScreen';
+import DatePickScreen from './Components/DatePickScreen';
+import WitnessedScreen from './Components/WitnessedScreen';
+import ThanksScreen from './Components/ThanksScreen';
+import axios from 'axios';
+import { Scene, Router, Actions, NavBar } from 'react-native-router-flux';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+render() {
+  return(
+    <Router>
+      <Scene key="root">
+        <Scene 
+          key="callout-map"
+          component={MapScreen}
+          hideNavBar={true}
+          initial 
+        />
+        <Scene 
+          key="witnessed"
+          component={WitnessedScreen}
+          hideNavBar={true}
+        />
+        <Scene 
+          key="datepick"
+          component={DatePickScreen}
+          hideNavBar={true}
+        />
+        <Scene 
+          key="thanks"
+          component={ThanksScreen}
+          hideNavBar={true}
+        />
+      </Scene>
+    </Router>
+    );
+  }
+}
+
+export default App;
