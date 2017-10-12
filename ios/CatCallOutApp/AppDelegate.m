0c4b4e3ae48713e42d13e73f70666cab970b01a7
Ginny Fahs <ginnyfahs@gmail.com>
2017-10-11 19:16:00 -0700

    🗝

--- ios/CatCallOutApp/AppDelegate.m
+++ ios/CatCallOutApp/AppDelegate.m
@@ -21,8 +21,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  [GMSServices provideAPIKey:@"GOOGLE_MAPS_API_KEY"];
-  [GMSPlacesClient provideAPIKey:@"GOOGLE_PLACES_API_KEY"];
+  [GMSServices provideAPIKey:@"AIzaSyClzw-1Fd1FePGdsHSutNP0w-7kXLFWLQ0"];
+  [GMSPlacesClient provideAPIKey:@"AIzaSyADHpneRQBBbmXzjx0_31K3k2_G-ekWiq0"];
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
